b5149dbfb61bb0a8a9ee32d11d99fa78ee641757
ALbertIM0427 <guang7226961@naver.com>
2024-09-05 01:39:12 +0900

    feat: 모든 그룹 조회 기능 구현

--- src/main/java/me/albert/todo/controller/GroupController.java
+++ src/main/java/me/albert/todo/controller/GroupController.java
@@ -1,11 +1,16 @@
 package me.albert.todo.controller;
 
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import me.albert.todo.controller.dto.request.GroupRequest;
 import me.albert.todo.service.GroupService;
+import me.albert.todo.service.dto.response.GroupResponse;
 import me.albert.todo.service.dto.response.IdResponse;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -33,9 +38,11 @@ public class GroupController {
             @PathVariable Long id,
             @CurrentUsername String username
     ) {
-        if (id == null) {
-            throw new IllegalArgumentException(GROUP_ID_REQUIRED);
-        }
         groupService.update(id, request.name(), request.description(), username);
+    }
+
+    @GetMapping("/groups")
+    public List<GroupResponse> list(@CurrentUsername String username, @PageableDefault(size = 20) Pageable pageable) {
+        return groupService.list(username, pageable);
     }
 }
