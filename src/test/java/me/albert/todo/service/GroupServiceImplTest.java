794f4c2f477a25c3dcd1d7b58eaa8a2f60697238
ALbertIM0427 <guang7226961@naver.com>
2024-09-05 00:05:43 +0900

    test: UserLoginRequestTest 테스트 코드 구현

--- src/test/java/me/albert/todo/service/GroupServiceImplTest.java
+++ src/test/java/me/albert/todo/service/GroupServiceImplTest.java
@@ -1,7 +1,6 @@
 package me.albert.todo.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
