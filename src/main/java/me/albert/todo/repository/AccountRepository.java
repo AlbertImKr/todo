e8dbe9173077d105811502c53a412789effa5179
ALbertIM0427 <guang7226961@naver.com>
2024-09-05 10:35:41 +0900

    refactor: Repository annotation를 사용하여 Repository 명시

--- src/main/java/me/albert/todo/repository/AccountRepository.java
+++ src/main/java/me/albert/todo/repository/AccountRepository.java
@@ -3,7 +3,9 @@ package me.albert.todo.repository;
 import java.util.Optional;
 import me.albert.todo.domain.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     boolean existsByUsername(String username);
