895324f0e5412936dd2ddd26da350a3aa88b8c07
ALbertIM0427 <guang7226961@naver.com>
2024-09-05 10:42:21 +0900

    docs: 주석 수정

--- src/main/java/me/albert/todo/service/AccountService.java
+++ src/main/java/me/albert/todo/service/AccountService.java
@@ -10,7 +10,7 @@ public interface AccountService {
      *
      * @param username 유저 이름
      * @param password 비밀번호
-     * @throws IllegalArgumentException 이미 존재하는 유저 이름일 경우 발생
+     * @throws me.albert.todo.exception.BusinessException 이미 존재하는 유저 이름일 경우 발생
      */
     void register(String username, String password);
 
@@ -20,7 +20,7 @@ public interface AccountService {
      * @param username 유저 이름
      * @param password 비밀번호
      * @return 토큰 정보
-     * @throws IllegalArgumentException 유저 이름이 존재하지 않거나 비밀번호가 일치하지 않을 경우 발생
+     * @throws me.albert.todo.exception.BusinessException 유저 이름이 존재하지 않거나 비밀번호가 일치하지 않을 경우 발생
      */
     TokensResponse login(String username, String password);
 
@@ -29,7 +29,7 @@ public interface AccountService {
      *
      * @param username 유저 이름
      * @return 사용자 정보
-     * @throws IllegalArgumentException 유저 이름이 존재하지 않을 경우 발생
+     * @throws me.albert.todo.exception.BusinessException 유저 이름이 존재하지 않을 경우 발생
      */
     Account findByUsername(String username);
 }
